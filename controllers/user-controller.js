26d3cbec4aa0c6660342832d564f80c743dca803
unknown <sabdullaeef87@gmail.com>
2021-03-17 05:06:44 +0100

    posting

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -2,13 +2,14 @@ const Users = require("../models/UserModel");
 
 const UserController = {
   register: async (req, res) => {
-    const { name, email, password } = req.body;
-    const newUser = new Users({
-      name,
-      password,
-      email,
-    });
-    await newUser.save();
+    res.send("POSTTTTIIINNGGGG");
+    // const { name, email, password } = req.body;
+    // const newUser = new Users({
+    //   name,
+    //   password,
+    //   email,
+    // });
+    // await newUser.save();
   },
 };
 
